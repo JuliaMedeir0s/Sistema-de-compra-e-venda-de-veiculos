d398aa9067eb73b6849e97c18a7841592cf1f472
gdelfino01 <145400944+gdelfino01@users.noreply.github.com>
2023-12-13 17:01:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -385,6 +385,118 @@ void exibirMenuBackup()
     printf("Escolha uma opção: ");
 }
 
+// Função que cria arquivo binario do arquivo veiculos_estoque
+void criaArquivoBinarioEstoque(char *veiculos_estoque, char *veiculos_binario)
+{
+    FILE *arquivoEstoque, *arquivoBinario;
+    char caractere;
+
+    // Abre o arquivo de texto para leitura
+    arquivoEstoque = fopen("veiculos_estoque.csv", "r+");
+
+    if (arquivoEstoque == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    arquivoBinario = fopen("veiculos_binario.csv", "wb");
+
+    if (arquivoEstoque == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copia letra por letra para o arquivo binário
+    while ((caractere = fgetc(arquivoEstoque)) != EOF)
+    {
+        fwrite(&caractere, sizeof(char), 1, arquivoBinario);
+    }
+
+    // Fecha o arquivo
+    fclose(arquivoEstoque);
+    fclose(arquivoBinario);
+
+    printf("Arquivo binario de estoque criado com sucesso\n");
+}
+
+// -------------------------------------------------------------------------------------
+
+// Função que cria arquivo binario do arquivo historico_compras
+void criaArquivoBinarioCompras(char *historico_compras, char *historico_compras_binario)
+{
+    FILE *arqHistoricoCompras, *arqHistoricoComprasBinario;
+    char caractere;
+
+    // Abre o arquivo de texto para leitura
+    arqHistoricoCompras = fopen("historico_compras.csv", "r+");
+
+    if (arqHistoricoCompras == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    arqHistoricoComprasBinario = fopen("historico_compras_binario.csv", "wb");
+
+    if (arqHistoricoComprasBinario == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copia letra por letra para o arquivo binário
+    while ((caractere = fgetc(arqHistoricoCompras)) != EOF)
+    {
+        fwrite(&caractere, sizeof(char), 1, arqHistoricoComprasBinario);
+    }
+
+    // Fecha o arquivo
+    fclose(arqHistoricoCompras);
+    fclose(arqHistoricoComprasBinario);
+
+    printf("Arquivo binario de historico de compras criado com sucesso\n");
+}
+
+// -------------------------------------------------------------------------------------
+
+// Função que cria arquivo binario do arquivo historico_compras
+void criaArquivoBinarioVendas(char *historico_vendas, char *historico_vendas_binario)
+{
+    FILE *arqHistoricoVendas, *arqHistoricoVendasBinario;
+    char caractere;
+
+    // Abre o arquivo de texto para leitura
+    arqHistoricoVendas = fopen("historico_vendas.csv", "r+");
+
+    if (arqHistoricoVendas == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    arqHistoricoVendasBinario = fopen("historico_vendas_binario.csv", "wb");
+
+    if (arqHistoricoVendasBinario == NULL)
+    {
+        perror("Erro ao abrir o arquivo de texto");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copia letra por letra para o arquivo binário
+    while ((caractere = fgetc(arqHistoricoVendas)) != EOF)
+    {
+        fwrite(&caractere, sizeof(char), 1, arqHistoricoVendasBinario);
+    }
+
+    // Fecha o arquivo
+    fclose(arqHistoricoVendas);
+    fclose(arqHistoricoVendasBinario);
+
+    printf("Arquivo binario de historico de vendas criado com sucesso\n");
+}
+
 int main()
 {
     // lista de arquivos usados o programa
@@ -393,6 +505,9 @@ int main()
     char *taxas = "marcas.csv";
     char *historicoCompras = "historico_compras.csv";
     char *historicoVendas = "historico_vendas.csv";
+    char *veiculosBinario = "veiculos_binario.csv";
+    char *historicoComprasBinario = "historico_compras_binario.csv";
+    char *historicoVendasBinario = "historico_vendas_binario.csv";
 
     char *parametroDeBusca; // Substitua pelo parâmetro desejado
     char *tipoParametro;
@@ -471,8 +586,19 @@ int main()
             // exibir opções de backup
             exibirMenuBackup();
             scanf("%d", &backup);
-            if (backup == 1)
+            if (backup == 1){
+                criaArquivoBinarioEstoque(estoque, veiculosBinario);
                 break;
+            }
+            if(backup == 2){
+                criaArquivoBinarioCompras(historicoCompras, historicoComprasBinario);
+                break;
+            }
+            if(backup == 3){
+                criaArquivoBinarioVendas(historicoVendas, historicoVendasBinario);
+                break;
+            }
+            break;
         case 7:
             printf("Saindo...\n");
             break;
